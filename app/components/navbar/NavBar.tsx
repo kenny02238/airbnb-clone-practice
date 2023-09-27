63d5d49f4688cbe447fb39becba3cf4e1beea7f1
Kenny <Kenny@batmobile.com.tw>
2023-09-27 21:05:27 +0800

    fix: 大型螢幕及windows開啟網站時跑版問題

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -11,7 +11,7 @@ interface NavBarProps {
 }
 const NavBar: React.FC<NavBarProps> = () => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div className="fixed w-full bg-white z-10 shadow-sm h-[180px]">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
